5a88c96319b109e2a7dcf70229b40ab5221654ac
Gonzalo Daniel <gonzalodaniel@google.com>
2025-03-13 22:19:07 -0500

    - Corriendo el prettier en 3 archivos

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 import vistas.VistaPaciente;
 
 public class App {
-    public static void main(String[] args) {
-        VistaPaciente vista = new VistaPaciente();
-        vista.iniciar();
-    }
+  public static void main(String[] args) {
+    VistaPaciente vista = new VistaPaciente();
+    vista.iniciar();
+  }
 }
